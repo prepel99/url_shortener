2754b2f9644193dae46b3cb70dd2c01dc718f5c8
Bohdan Prepelytsia <prepel99@gmail.com>
2021-01-26 23:06:44 +0200

    added handling of format for url

--- app/controllers/url_shortener_controller.rb
+++ app/controllers/url_shortener_controller.rb
@@ -2,7 +2,7 @@ class UrlShortenerController < ApplicationController
   def create
     url = JSON.parse(request.raw_post)['url']
     ## todo: handle if url is empty
-    render json: { url: url, short_url: ShortenerHelper::Shortener.add_url(url) }
+    render json: ShortenerHelper::Shortener.short_url(url)
   end
 
   def redirect
--- app/helpers/shortener_helper.rb
+++ app/helpers/shortener_helper.rb
@@ -4,12 +4,22 @@ module ShortenerHelper
   class Shortener
     @@short_urls = {}
 
-    def self.add_url(url)
-      @@short_urls[url] = SecureRandom.alphanumeric(6)
+    def self.short_url(url)
+      formatted_url = handle_format(url)
+      @@short_urls[formatted_url] = SecureRandom.alphanumeric(6)
+      { url: formatted_url, short_url: @@short_urls[formatted_url] }
     end
 
     def self.short_urls
       @@short_urls
     end
+
+    private
+
+      def self.handle_format(url)
+        return url if url.include?('http://') || url.include?('https://')
+
+        'http://' + url
+      end
   end
 end
